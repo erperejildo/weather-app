cacb5e5ab422f00eba5b6f89b286ca637050bcad
Daniel <d15_1_89@msn.com>
2024-07-15 13:17:05 +0200

    themes ligth & dark

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import { Details } from './components/details/detaisl';
 import { Logo } from './components/logo/logo';
 import { Search } from './components/search/search';
 import { ForecastContext } from './ForecastContext';
+import { Background } from './interfaces/Background';
+import { weatherBgMap } from './utils/backgrounds';
 
 function App() {
   const [shrinkHeader, setShrinkHeader] = useState(false);
+  const [theme, setTheme] = useState('light');
   const context = useContext(ForecastContext);
 
   if (!context) return;
+
+  useEffect(() => {
+    if (context?.forecast) {
+      const icon = context.forecast.currentConditions.icon;
+      const weatherBg: Background =
+        weatherBgMap[icon] || weatherBgMap['default'];
+      document.body.style.backgroundImage = `url(backgrounds/${weatherBg.label})`;
+      setTheme(weatherBg.theme);
+    }
+  }, [context.forecast]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -30,7 +43,7 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <div className={getHeaderClassName()}>
         <Logo />
         {!shrinkHeader && <Search />}
