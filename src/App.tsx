9e3735938c3e84f6d20958e5847de0d9c68440c7
Daniel <d15_1_89@msn.com>
2024-07-15 13:05:47 +0200

    shrink header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css';
+import { useContext, useEffect, useState } from 'react';
+import './App.scss';
 import { Logo } from './components/logo/logo';
+import { ForecastContext } from './ForecastContext';
 
 function App() {
+  const [shrinkHeader, setShrinkHeader] = useState(false);
+  const context = useContext(ForecastContext);
+
+  if (!context) return;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setShrinkHeader(position > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [location]);
+
+  const getHeaderClassName = () => {
+    return shrinkHeader ? 'fixed-wrapper shrink' : 'fixed-wrapper';
+  };
+
   return (
-    <div>
-      <Logo />
+    <div className="app">
+      <div className={getHeaderClassName()}>
+        <Logo />
+      </div>
     </div>
   );
 }
