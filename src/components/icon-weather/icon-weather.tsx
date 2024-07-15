fa8c02a4e9d09ff06c6b91f904a4eedcd375aaed
Daniel <d15_1_89@msn.com>
2024-07-15 13:11:54 +0200

    icon component

---
+++ src/components/icon-weather/icon-weather.tsx
@@ -0,0 +1,20 @@
+// WeatherComponent.tsx
+import { FC } from 'react';
+import { MdErrorOutline } from 'react-icons/md';
+import weatherIconMap, { isWeatherIconName } from '../../utils/icons';
+
+type WeatherProps = {
+  icon: string;
+};
+
+const WeatherComponent: FC<WeatherProps> = ({ icon }) => {
+  if (!isWeatherIconName(icon)) {
+    return <MdErrorOutline />;
+  }
+
+  const IconComponent = weatherIconMap[icon];
+
+  return <IconComponent />;
+};
+
+export default WeatherComponent;
